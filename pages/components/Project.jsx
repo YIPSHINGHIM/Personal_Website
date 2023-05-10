e47b11304b33a4eafdedb865afec16e5a9f68de6
Shing Him Yip <shing.yip.2020@live.rhul.ac.uk>
2023-05-10 16:46:04 +0100

    added error handle

--- pages/components/Project.jsx
+++ pages/components/Project.jsx
@@ -1,15 +1,22 @@
-function Project({ Name, Details, Tools = [] }) {
+import Link from "next/link";
+
+function Project({ Name, Details, githubLink = 'https://github.com/YIPSHINGHIM', Tools = [] }) {
   const ToolsIUsed = Tools.map((item) => item);
 
+  
+
   return (
-    <div className="text-center shadow-lg p-10 w-85 rounded-xl dark:bg-gray-700">
-      <h3 className="text-lg font-medium pt-8 pb-2  ">{Name}</h3>
-      <p className="py-2 flex">{Details}</p>
-      <h4 className="py-4 text-teal-600">Tools I Use</h4>
-      <div className="ToolIUsed p-2 text-6xl flex flex-wrap justify-center ">
-        {ToolsIUsed}
+    // TODO need to update the link
+    <Link href={githubLink} target="_blank">
+      <div className="text-center shadow-lg p-10 w-85 rounded-xl dark:bg-gray-700">
+        <h3 className="text-lg font-medium pt-8 pb-2  ">{Name}</h3>
+        <p className="py-2 flex">{Details}</p>
+        <h4 className="py-4 text-teal-600">Tools I Use</h4>
+        <div className="ToolIUsed p-2 text-6xl flex flex-wrap justify-center ">
+          {ToolsIUsed}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- pages/components/ProjectList.jsx
+++ pages/components/ProjectList.jsx
@@ -11,6 +11,7 @@ const project1 = {
   Name: "Todo List",
   Details:
     "web application using React and Django, with features including adding, editing, and deleting tasks, and fetching data from a REST API.",
+  githubLink: "https://github.com/YIPSHINGHIM/TodoList_using_React_and_Django",
   Tools: [
     <FaReact key="React" />,
     <SiDjango key="Django" />,
@@ -25,6 +26,7 @@ const project2 = {
   Name: "Restaurant Management System",
   Details:
     "Restaurant Management System allowed users to manage table bookings, order placement, and track inventory in real-time, resulting in improved operational efficiency and customer satisfaction.",
+  githubLink: "https://github.com/YIPSHINGHIM/Team_project",
   Tools: [
     <SiFlask key="Flask" />,
     <AiFillHtml5 key="HTML" />,
@@ -39,6 +41,7 @@ const project3 = {
   Name: "Personal Website",
   Details:
     "Designed and developed a responsive and visually personal website using Next.js and Tailwind CSS, showcasing my skills, projects, and experiences while providing a smooth user experience.",
+  githubLink: "https://github.com/YIPSHINGHIM/Personal_Website",
   Tools: [<TbBrandNextjs key="Next.Js" />, <SiTailwindcss key="Tailwindcss" />],
 };
 
@@ -46,6 +49,7 @@ const project4 = {
   Name: "Stock Price Message Bot",
   Details:
     "Stock price message bot project using Python, which involved retrieving live stock prices through Alpha Vantage API and delivering customized price alerts to users via Telegram",
+  githubLink: "https://github.com/YIPSHINGHIM/Stock-price-message-bot",
   Tools: [<IoLogoPython key="Python" />],
 };
 
@@ -53,7 +57,22 @@ const project5 = {
   Name: "Auto Video Uploading Bot",
   Details:
     "Auto Video Uploading Bot using Python and Selenium to automate the process of uploading videos to multiple social media platforms, saving hours of manual work.",
+  githubLink: "https://github.com/YIPSHINGHIM/upload_video_bot",
   Tools: [<SiSelenium key="Selenium" />],
+};
+
+const project6 = {
+  Name: "VaR calculation web app",
+  Details:
+    "The Value at Risk (VaR) Web Application provides users with a user-friendly interface to accurately calculate and assess their financial risk, contributing to more informed investment decisions and effective risk management.",
+  githubLink: "https://github.com/YIPSHINGHIM/upload_video_bot",
+  Tools: [
+    <FaReact key="React" />,
+    <SiDjango key="Django" />,
+    <AiFillHtml5 key="HTML" />,
+    <DiCss3 key="CSS" />,
+    <IoLogoJavascript key="JavaScript" />,
+  ],
 };
 
 export default class ProjectList extends Component {
@@ -68,6 +87,7 @@ export default class ProjectList extends Component {
             <Project {...project3} />
             <Project {...project4} />
             <Project {...project5} />
+            <Project {...project6} />
           </div>
         </div>
       </div>
